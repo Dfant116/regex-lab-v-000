7da9753aeb99fbb77f2a584942301e3e8ef5ba04
Dalton Fant <daltonfant16@gmail.com>
2018-02-22 12:04:50 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,13 @@
 def starts_with_a_vowel?(word)
-
+  if word.match(/^[aeiouAEIOU]+\w/)
+    TRUE
+  else
+    false 
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  word.scan(/w+un+ing/)
 end
 
 def words_five_letters_long(text)
